e4446c921e9aabbe1f64792205fbe0298055b4ba
leopoldo <villaleoo@hotmail.com>
2024-10-06 18:43:11 -0300

    [SIDEBAR] reemplazado divs de categorias por <a> y mejorado separacion de categorias

--- src/js/script.js
+++ src/js/script.js
@@ -302,6 +302,10 @@
                         <g id="SVGRepo_iconCarrier">
                         <path d="M-260.9,327.8c0-10.3,9.2-14,19.5-14c10.3,0,21.3,3.2,21.3,3.2l6.6-39.2c0,0-14-4.8-47.4-4.8c-20.5,0-32.4,7.8-41.1,19.3c-8.2,10.9-8.5,28.4-8.5,39.7v25.7H-337V396h26.5v133h49.6V396h39.3l2.9-38.3h-42.2V327.8z"/></g>
                         </svg>`,
+                SVG_PRICE:`<svg fill=${color} viewBox="-2 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg" transform="rotate(90)">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.49400000000000005"/><g id="SVGRepo_iconCarrier">
+                        <path d="m13.842 11.52-4.389 4.388a1.112 1.112 0 0 1-1.567 0l-6.28-6.28a3.027 3.027 0 0 1-.771-1.892l.043-3.681A1.141 1.141 0 0 1 2 2.935L5.67 2.9a3.04 3.04 0 0 1 1.892.773l6.28 6.28a1.112 1.112 0 0 1 0 1.567zM3.826 5.133a.792.792 0 1 0-.792.792.792.792 0 0 0 .792-.792zm6.594 7.348a.554.554 0 0 0 0-.784l-.401-.401a2.53 2.53 0 0 0 .35-.83 1.565 1.565 0 0 0-.397-1.503 1.59 1.59 0 0 0-1.017-.46 2.14 2.14 0 0 0-.75.085h-.002a2.444 2.444 0 0 0-.59.277H7.61a2.677 2.677 0 0 0-.438.357 2.043 2.043 0 0 1-.259.22 1.29 1.29 0 0 1-.329.17h-.002a.835.835 0 0 1-.338.038h-.002a.53.53 0 0 1-.314-.136.539.539 0 0 1-.106-.534 1.54 1.54 0 0 1 .41-.71 1.632 1.632 0 0 1 .23-.165l.03-.019a1.783 1.783 0 0 1 .322-.155.942.942 0 0 1 .325-.06.554.554 0 0 0 0-1.108h-.001a2.058 2.058 0 0 0-.717.132 2.846 2.846 0 0 0-.529.26l-.01.006-.398-.4a.554.554 0 1 0-.784.785l.388.387a2.513 2.513 0 0 0-.347.803 1.644 1.644 0 0 0 .404 1.561 1.622 1.622 0 0 0 .983.456 1.922 1.922 0 0 0 .805-.089 2.372 2.372 0 0 0 .624-.319 3.142 3.142 0 0 0 .398-.339 1.569 1.569 0 0 1 .256-.208 1.381 1.381 0 0 1 .32-.151 1.023 1.023 0 0 1 .348-.038.485.485 0 0 1 .308.139c.05.049.165.165.097.488a1.558 1.558 0 0 1-.413.729 2.476 2.476 0 0 1-.28.219 1.727 1.727 0 0 1-.306.157.687.687 0 0 1-.32.042.554.554 0 1 0-.08 1.106c.052.004.103.005.152.005a1.723 1.723 0 0 0 .685-.134 2.678 2.678 0 0 0 .507-.27l.01-.007.397.398a.555.555 0 0 0 .783 0z"/></g>
+                        </svg>`
                     
             }
            
@@ -353,7 +357,6 @@
             header.innerHTML=template;
     
            
-    
             return header;
         }
     
@@ -449,6 +452,8 @@
         getComponent(){
             const sidebar = document.createElement("sidebar");
             sidebar.className="sidebar-categories";
+            const hr = document.createElement("hr");
+            hr.className="separator-categories";
            
             //por cada elemento en la lista creo un div (category-item) y se lo agrego/apendo al sidebar
             
@@ -456,9 +461,9 @@
                 sidebar.appendChild( this.#createCategoryItemComponent(categ) );
             });
            
+            sidebar.insertBefore(hr,sidebar.children[4]);
             sidebar.style.overflow='hidden';
            
-
             return sidebar;
         }
 
@@ -477,16 +482,19 @@
 
         //crea el contenedor de cada categoria solo con el icono y le asigna los eventos para que al ser clickeado pueda ser identificado con data-value
         #createCategoryItemComponent(categoryObj){
-            const catContainer= document.createElement("div");
+            const catContainer= document.createElement("a");
+            
             catContainer.className=`category-item c-${Utils.replaceSpaces(categoryObj.name)} ${
                 this.#activeCateg === null && categoryObj.name === "inicio" ? "c-active":
-                categoryObj.name == this.#activeCateg ? "c-active":''}`
-
-            catContainer.setAttribute("data-value", `${Utils.replaceSpaces(categoryObj.name)}`);
-
+                categoryObj.name == this.#activeCateg ? "c-active":''}`;
+                
+            catContainer.href=`#${Utils.replaceSpaces(categoryObj.name)}`;
+                
+                
             const template = `${Utils.SVGTemplate( categoryObj.iconSVG,"category-svg" )}`;
-
+                
             catContainer.innerHTML=template;
+           
 
             catContainer.addEventListener("click", (e)=>{
                 e.preventDefault();
@@ -568,27 +576,23 @@
         }
 
        
-
-      
         //captura en la categoria que se clickea a traves del data-value
         #handleClickCategory(element){
-            const id = element.getAttribute("data-value");
+            const hrefValue= element.getAttribute("href");
             const active = document.querySelector(".c-active");
+
             active.classList.remove("c-active");
-            this.#activeCateg=Utils.unReplaceSpaces(id);
+            this.#activeCateg=Utils.unReplaceSpaces(hrefValue.slice(1));
 
             element.classList.add("c-active");
 
-            
-            console.log("clickeaste en: "+id);
+            console.log("clickeaste en: "+hrefValue);
            
         }
 
 
     }
     
-
-
     //este controla los eventos de la tarjeta, no los del avatar (porque avatar es del header)
     //aca ya asumis que el usuario esta logueado. no hace falta controlar la sesion
     class SessionCard {
@@ -735,23 +739,46 @@
 
     }
 
-    /*tengo que hacer game que contiene data para > card que sirve para hacer> carrousel */
 
-    class Game {
-        #id;
-        #title;
-        #frontImg;
-        #category;
-        #media;
-        #description;
-        #controllers;
-        #comments;
-        #price;
-        #isPay;
+    class Card{
+        #game;
+
+        constructor(game){
+            this.#game=game;
+        }
+
+        getSmallCard(){
+
+        }
 
 
+        #createCustomArticle(size){
+            const isPay= game.esPago;
+            const article = document.createElement("article");
+            article.className=`card-${size}`;
+            article.style.backgroundImage
+
+            if(isPay){
+
+                //todo lo que haya que ocultar con hover ponerlo en un div contenedor
+            }
+            
 
 
+
+
+        }
+
+        #renderPrice(){
+
+            const priceTemplate = `<div class="price">
+                                ${Utils.SVGTemplate(Utils.customSVG("SVG_PRICE","#FAFAFA"))}
+                                <p>${game.precio}</p>
+                              </div>`;
+
+            return priceTemplate;
+
+        }
     }
 
 
